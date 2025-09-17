dbf21ff4fa3b75f45270fe7dbe61db7a736c410a
JonasCandido <jonasdcandido210@gmail.com>
2025-09-17 18:33:13 -0300

    Finish basic-html-site project

--- basic-html-site/src/contact/index.jsx
+++ basic-html-site/src/contact/index.jsx
@@ -4,10 +4,10 @@ const Contact = () => {
         <h1>Contact Me</h1>
           <form action="#" method="POST">
             <label for="name">Name:</label>
-            <input type="text" id="name" name="name" required />
+            <input type="text" id="name" name="name" autocomplete="name"  required />
 
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required />
+            <input type="email" id="email" name="email" autocomplete="email" required />
 
             <label for="message">Message:</label>
             <textarea id="message" name="message" required></textarea>
